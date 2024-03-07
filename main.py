70168663b008a6dc261420145855d21b160080ad
andrew-harris <25672844+andrew-harris@users.noreply.github.com>
2024-03-07 19:55:01 +0000

    All characters in dictionary but value not incrementing

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def main():
 
+    word_list = []
+    breakdown = {}
+
+
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
+    word_list = file_contents.split()
 
-    print(file_contents)
+    for word in word_list:
+        character = []
+        for char in word:
+            if char.isalpha() == True:
+                breakdown[char] = 1
+    print(breakdown)
+
 main()
